524a938bb29b63ecfd97866c317573eeccc6a876
Alexander Faust <bardiir@gmail.com>
2018-04-15 12:14:15 +0200

    load html from template

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,17 +1,40 @@
 // Initial welcome page. Delete the following line to remove it.
-'use strict';const styles=document.createElement('style');styles.innerText=`@import url(https://unpkg.com/bulma/css/bulma.css);.empty{display:flex;flex-direction:column;justify-content:center;height:100vh;position:relative}.footer{bottom:0;font-size:13px;left:50%;opacity:.9;position:absolute;transform:translateX(-50%);width:100%}`;const vueScript=document.createElement('script');vueScript.setAttribute('type','text/javascript'),vueScript.setAttribute('src','https://unpkg.com/vue'),vueScript.onload=init,document.head.appendChild(vueScript),document.head.appendChild(styles);function init(){Vue.config.devtools=false,Vue.config.productionTip=false,new Vue({data:{versions:{electron:process.versions.electron,electronWebpack:require('electron-webpack/package.json').version}},methods:{open(b){require('electron').shell.openExternal(b)}},template:`<div><div class=empty><p class="title is-5">Welcome to VHS (Video HTTP Streamer)!<p class=subtitle is-5>Get started now and take advantage of the great documentation at hand.<div class=empty-action><button @click="open('https://webpack.electron.build')"class="button is-primary">Documentation</button> <button @click="open('https://electron.atom.io/docs/')"class="button is-info">Electron</button><br><ul class=breadcrumb><li class=breadcrumb-item>electron-webpack v{{ versions.electronWebpack }}</li><li class=breadcrumb-item>electron v{{ versions.electron }}</li></ul></div><p class=footer>This intitial landing page can be easily removed from <code>src/renderer/index.js</code>.</p><link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css" integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" crossorigin="anonymous"><div class="file has-name is-boxed is-primary is-centered">
-  <label class="file-label">
-    <input class="file-input" type="file" name="resume">
-    <span class="file-cta">
-      <span class="file-icon">
-        <i class="fas fa-upload"></i>
-      </span>
-      <span class="file-label">
-        Choose a video…
-      </span>
-    </span>
-    <span class="file-name">
-      video-to-convert.mkv
-    </span>
-  </label>
-</div></div></div>`}).$mount('#app')}
+'use strict';
+
+import * as path from 'path'
+var fs = require('fs');
+
+
+fs.readFile(path.join(__dirname, 'style.css'), 'utf-8', (err, data) => {
+	var styles = document.createElement('style')
+	styles.innerText = data;
+	document.head.appendChild(styles);
+});
+
+const vueScript=document.createElement('script');
+
+vueScript.setAttribute('type','text/javascript'),
+vueScript.setAttribute('src','https://unpkg.com/vue'),
+vueScript.onload=init,
+document.head.appendChild(vueScript);
+
+function init(){
+	fs.readFile(path.join(__dirname, 'body.html'), 'utf-8', (err, data) => {
+		
+		Vue.config.devtools=false,
+		Vue.config.productionTip=false,
+		new Vue({
+			data:{
+				versions:{
+					electron:process.versions.electron,electronWebpack:require('electron-webpack/package.json').version
+				}
+			},
+			methods:{
+				open(b){
+					require('electron').shell.openExternal(b)
+				}
+			},
+			template: data
+		}).$mount('#app')
+	});
+}
